82bbaaaaa2db6248a9beea388f488203ce860aaa
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-16 21:32:53 -0600

    Adapter: Configuración restante

--- app/src/main/java/com/example/users_sp_da/UserAdapter.kt
+++ app/src/main/java/com/example/users_sp_da/UserAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.users_sp_da
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,16 +9,25 @@ import com.example.users_sp_da.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = users.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = users.get(position)
+
+        with(holder){
+            binding.tvOrder.text = user.id.toString()
+            binding.tvName.text = user.nombre
+        }
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
