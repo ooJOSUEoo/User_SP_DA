eca57755b592496750774d4c4ce08682882f3ebf
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-18 19:45:06 -0600

    Vista alternativa

--- app/src/main/java/com/example/users_sp_da/UserAdapter.kt
+++ app/src/main/java/com/example/users_sp_da/UserAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.users_sp_da.databinding.ItemUserBinding
+import com.example.users_sp_da.databinding.ItemUserAltBinding
 
 class UserAdapter(private val users: List<User>, private val listener: OnClickListener) :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
@@ -15,7 +15,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
 
-        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user_alt, parent, false)
 
         return ViewHolder(view)
     }
@@ -39,7 +39,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val binding = ItemUserBinding.bind(view)
+        val binding = ItemUserAltBinding.bind(view)
 
         fun setListener(user: User, position: Int){
             binding.root.setOnClickListener{listener.onClick(user, position)}
