98e59eb0071a1b38438e31bdadfaa055700bb3be
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-18 19:06:59 -0600

    Dialogos con Material Design

--- app/src/main/java/com/example/users_sp_da/MainActivity.kt
+++ app/src/main/java/com/example/users_sp_da/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.users_sp_da
 
 import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.users_sp_da.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
@@ -26,7 +28,15 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         val isFirstTime = preferences.getBoolean(getString(R.string.sp_first_time),true)
         Log.i("SP","${getString(R.string.sp_first_time)} = $isFirstTime")
 
-        preferences.edit().putBoolean(getString(R.string.sp_first_time),false).commit()
+        if(isFirstTime) {
+            MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.dialog_confirm, DialogInterface.OnClickListener { dialogInterface, i ->
+                    preferences.edit().putBoolean(getString(R.string.sp_first_time), false).commit()
+                })
+                .setNegativeButton("Cancelar",null)
+                .show()
+        }
 
         userAdapter = UserAdapter(getUsers(), this)
         linearLayoutManager = LinearLayoutManager(this)
